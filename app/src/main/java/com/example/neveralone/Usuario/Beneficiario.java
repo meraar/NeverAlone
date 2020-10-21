4560867c5639f8fdb0199a7383399d0377f2651e
muhammad.meraj.arshad <muhammad.meraj.arshad@est.fib.upc.edu>
2020-10-21 19:38:06 +0200

    Clases de Usuario

---
+++ app/src/main/java/com/example/neveralone/Usuario/Beneficiario.java
@@ -0,0 +1,15 @@
+package com.example.neveralone.Usuario;
+
+public class Beneficiario extends Usuario {
+    private String direccion;
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+
+}
