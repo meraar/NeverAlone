387ee704584b67b44d864267c9323add6b9e1034
sufang <sufang.zhou@estudiant.upc.edu>
2020-10-21 14:10:03 +0200

    Creo la carpeta activity con las clases para hacer la alta usuario

--- app/src/main/java/com/example/neveralone/Activity/MainActivity.java
+++ app/src/main/java/com/example/neveralone/Activity/MainActivity.java
@@ -8,9 +8,5 @@ import com.example.neveralone.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+
 }
