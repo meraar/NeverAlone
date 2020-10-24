bc7d22adfef03746315336bcd5ea76d27f348b8a
muhammad.meraj.arshad <muhammad.meraj.arshad@est.fib.upc.edu>
2020-10-24 21:05:23 +0200

    Update

--- app/src/main/java/com/example/neveralone/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/neveralone/Activity/RegisterActivity.java
@@ -3,6 +3,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -26,25 +27,112 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 //import res.layout.*;
 import com.example.neveralone.R;
+import com.example.neveralone.Usuario.*;
 public class RegisterActivity extends AppCompatActivity {
     //private EditText idRegistroNombre, idRegistroCorreo, idRegistroContrasen, idRegistroContrasenaRepetida;
-    //private EditText codigo_postal, vulnerabilidad, ciudad_trabajo;
+    private EditText codigo_postal, vulnerabilidad, ciudad_trabajo;
     private RadioButton idRadioButtonBeneficiario, idRadioButtonVoluntario;
-    private Button idRegistroRegistrar;
     private FirebaseAuth mAuth;
     private FirebaseDatabase database;
     private EditText txtNombre, txtCorreo, txtContrasena, txtContrasenaRepetida;
+    private EditText txtpostal, txtvulnerabilidad, txtciudad;
     private Button btnRegistrar;
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         txtNombre = findViewById(R.id.idRegistroNombre);
         txtCorreo = findViewById(R.id.idRegistroCorreo);
+        //txtApellidos = findViewById(R.id.)
         txtContrasena = findViewById(R.id.idRegistroContrasena);
         txtContrasenaRepetida = findViewById(R.id.idRegistroContrasenaRepetida);
         btnRegistrar = findViewById(R.id.idRegistroRegistrar);
+        txtpostal = findViewById(R.id.codigo_postal);
+        txtvulnerabilidad = findViewById(R.id.vulnerabilidad);
+        txtciudad = findViewById(R.id.ciudad_trabajo);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                final String correo = txtCorreo.getText().toString();
+                final String nombre = txtNombre.getText().toString();
+                //final String apellidos = txtApellidos.getTest().toString(); // Este campo falta añadir en front
+                if(isValidEmail(correo) && validarContrasena() && validarNombre(nombre)){
+                    String contrasena = txtContrasena.getText().toString();
+                    mAuth.createUserWithEmailAndPassword(correo, contrasena)
+                            .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(RegisterActivity.this, "Se registro correctamente", Toast.LENGTH_SHORT).show();
+                                        Usuario usuario = new Usuario(correo, nombre, " ");
+                                        usuario.setEmail(correo);
+                                        //usuario.setApellidos(apellidos);
+                                        usuario.setNombre(nombre);
+                                        FirebaseUser currentUser = mAuth.getCurrentUser();
+                                        DatabaseReference reference = database.getReference("Usuarios/"+currentUser.getUid());
+                                        reference.setValue(usuario);
+                                        txtNombre.setText("");
+                                        txtCorreo.setText("");
+                                        txtContrasena.setText("");
+                                        txtContrasenaRepetida.setText("");
+                                        startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+                                        finish();
+                                    } else {
+                                        Toast.makeText(RegisterActivity.this, "Error al registrarse.", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
 
     }
+
+
+
+
+
+    private boolean isValidEmail(String email) {
+        boolean valid = !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+        if(!valid) Toast.makeText(this, "Introduce un correo valido.", Toast.LENGTH_SHORT).show();
+        return valid;
+    }
+
+    public boolean validarContrasena(){
+        String contrasena, contrasenaRepetida;
+        contrasena = txtContrasena.getText().toString();
+        contrasenaRepetida = txtContrasenaRepetida.getText().toString();
+        if(contrasena.equals(contrasenaRepetida)){
+            if(contrasena.length()>=6 && contrasena.length() <16){
+                return true;
+            } else{
+                Toast.makeText(this, "La contraseña ha de contener entre 6 y 15 carácteres.", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        } else {
+            Toast.makeText(this, "Las contraseñas no coinciden.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    public boolean validarNombre(String nombre){
+        if(nombre.isEmpty()){
+            Toast.makeText(this, "El nombre no puede estar vacio.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else return true;
+    }
+/**
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }**/
 
 
 
--- app/src/main/java/com/example/neveralone/Usuario/Usuario.java
+++ app/src/main/java/com/example/neveralone/Usuario/Usuario.java
@@ -1,5 +1,21 @@
 package com.example.neveralone.Usuario;
 
+
+import android.content.Intent;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.example.neveralone.Activity.LoginActivity;
+import com.example.neveralone.Activity.RegisterActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Usuario {
     private String email;
     private String nombre;
@@ -37,6 +53,14 @@ public class Usuario {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public Usuario(String correo, String nombre, String apellidos){
+        this.setEmail(correo);
+        this.setNombre(nombre);
+        this.setApellidos(apellidos);
+    }
+
+
 
 
 
