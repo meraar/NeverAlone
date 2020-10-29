7185b9e2d59c8cbfb8e404ae5cc99b1d048b891f
Erika <erika.salome.morales@est.fib.upc.edu>
2020-10-30 00:06:16 +0100

    "Updated activity_home.xml layout"

--- app/src/main/java/com/example/neveralone/UI/Home.java
+++ app/src/main/java/com/example/neveralone/UI/Home.java
@@ -1,6 +1,7 @@
 package com.example.neveralone.UI;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +12,10 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    /*TODO he añadido la función que está asociada al botón, cuando le das a cerrar sesión. Está será la pagina de inicio tras iniciar sesión.*/
+    public void signOut(View view) {
+
     }
 }
