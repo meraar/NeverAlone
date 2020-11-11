bdf07147c1ec7dbd5a7546f5b3ff2a1415604294
Marcel <marcel.dfga@gmail.com>
2020-11-11 19:17:01 +0100

    Añade un layout para crear una peticion que se guarda en FireBase
    Se comprueba que los campos requeridos esten rellenados y cumplan con los criterios de aceptacion

--- app/src/androidTest/java/com/example/neveralone/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/neveralone/ExampleInstrumentedTest.java
@@ -2,12 +2,22 @@ package com.example.neveralone;
 
 import android.content.Context;
 
+import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import com.example.neveralone.Activity.Peticiones.CrearPeticionActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +26,15 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityTestRule<CrearPeticionActivity> activityRule
+            = new ActivityTestRule<>(CrearPeticionActivity.class);
+
     @Test
     public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.neveralone", appContext.getPackageName());
+
     }
+
 }
---
+++ app/src/main/java/com/example/neveralone/Peticion/Peticion.java
@@ -0,0 +1,97 @@
+package com.example.neveralone.Peticion;
+
+import com.google.firebase.database.Exclude;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Peticion {
+
+    private String user;
+    private String uid;
+    private String descripcion;
+    private String categoria;
+    private String fecha;
+    private String hora;
+    private Estado estado;
+
+    public Peticion(){
+        this.user           = null;
+        this.descripcion    = null;
+        this.categoria      = null;
+        this.fecha          = null;
+        this.hora           = null;
+        this.estado         = null;
+    }
+
+    public Peticion(String user, String uid, String categoria, String fecha, String hora, String descripcion){
+        this.user           = user;
+        this.uid            = uid;
+        this.descripcion    = descripcion;
+        this.categoria      = categoria;
+        this.fecha          = fecha;
+        this.hora           = hora;
+        this.estado         = Estado.PENDIENTE;
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("uid", uid);
+        result.put("usuario", user);
+        result.put("categoria", categoria);
+        result.put("fecha", fecha);
+        result.put("hora", hora);
+        result.put("descripcion", descripcion);
+        result.put("estado", estado.toString());
+        return result;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+}
