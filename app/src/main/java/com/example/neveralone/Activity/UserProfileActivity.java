1d1f33fe64e76c2162c7f9bcb63461dbc0c730a4
muhammad.meraj.arshad <muhammad.meraj.arshad@est.fib.upc.edu>
2020-11-16 23:13:52 +0100

    Perfil

--- app/src/main/java/com/example/neveralone/Activity/UserProfileActivity.java
+++ app/src/main/java/com/example/neveralone/Activity/UserProfileActivity.java
@@ -12,10 +12,13 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.neveralone.R;
 import com.example.neveralone.Usuario.Usuario;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -34,6 +37,7 @@ public class UserProfileActivity<InputLayout> extends AppCompatActivity {
     private FirebaseUser user;
     private DatabaseReference reference;
     private Usuario usuario;
+    private String id;
 
 
 
@@ -61,7 +65,7 @@ public class UserProfileActivity<InputLayout> extends AppCompatActivity {
     private void initialize(){
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         user = firebaseAuth.getCurrentUser();
-        String id = user.getUid();
+        id = user.getUid();
         reference = FirebaseDatabase.getInstance().getReference();
         reference.child("Usuarios").child(id).addListenerForSingleValueEvent(
                 new ValueEventListener() {
@@ -74,6 +78,7 @@ public class UserProfileActivity<InputLayout> extends AppCompatActivity {
                             dnitxt.setText(usuario.getDni());
                             dnitxt.setEnabled(false);
                             emailtxt.setText(usuario.getEmail());
+                            emailtxt.setEnabled(false);
                             codigo_postaltxt.setText(usuario.getCodigopostal());
 
                             if (!usuario.getVoluntario()){
@@ -103,7 +108,7 @@ public class UserProfileActivity<InputLayout> extends AppCompatActivity {
 
                             }
                         } else {
-                            //Log.d("MyApa", "Hay un error");
+                            Toast.makeText(UserProfileActivity.this, "No hay información del usuario", Toast.LENGTH_SHORT).show();
                         }
                     }
 
@@ -114,11 +119,61 @@ public class UserProfileActivity<InputLayout> extends AppCompatActivity {
                 });
     }
 
-
     public void UpdateProfile(View view) {
-        FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(UserProfileActivity.this, LoginActivity.class));
-        finish();
+        if(isAnyUpdate()){
+            Toast.makeText(UserProfileActivity.this, "Se ha actualizado el perfil correctamente.", Toast.LENGTH_SHORT).show();
+        }else Toast.makeText(UserProfileActivity.this, "No hay ninguna modificación en los datos, por tanto no se puede actualizar el perfil", Toast.LENGTH_SHORT).show();
     }
+
+    private boolean isAnyUpdate() {
+        boolean name, surname, piso, direction, postal_code;
+        name = false;
+        surname = false;
+        piso = false;
+        direction = false;
+        postal_code = false;
+
+        String nombre = nombretxt.getText().toString();
+        if(!usuario.getNombre().equals(nombre)){
+            reference.child("Usuarios").child(id).child("nombre").setValue(nombre);
+            usuario.setNombre(nombre); // Utilizo la variable global para tener constancia del cambio, y por si el usuario vuelve a intentar cambiarlo.
+            Toast.makeText(UserProfileActivity.this, "Nombre = "+ usuario.getNombre() +" .", Toast.LENGTH_SHORT).show();
+            name = true;
+        }
+
+        String apellido = apellidotxt.getText().toString();
+        if(!usuario.getApellidos().equals(apellido)){
+            reference.child("Usuarios").child(id).child("apellidos").setValue(apellido);
+            usuario.setApellidos(apellido);
+            surname = true;
+        }
+
+        String codigo_postal = codigo_postaltxt.getText().toString();
+        if (codigo_postal.length() != 5) {
+            codigo_postaltxt.setError("El código postal debe ser de 5 dígitos");
+        } else if(!usuario.getCodigopostal().equals(codigo_postal)){
+                reference.child("Usuarios").child(id).child("codigopostal").setValue(codigo_postal);
+                usuario.setCodigopostal(codigo_postal);
+                postal_code = true;
+        }
+
+        String direccion = direcciontxt.getText().toString();
+        if(!usuario.getDireccion().equals(direccion)){
+            reference.child("Usuarios").child(id).child("direccion").setValue(direccion);
+            usuario.setDireccion(direccion);
+            direction = true;
+        }
+
+        String piso_puerta = piso_puertatxt.getText().toString();
+        if(!usuario.getPiso_puerta().equals(piso_puerta)){
+            reference.child("Usuarios").child(id).child("piso_puerta").setValue(piso_puerta);
+            usuario.setPiso_puerta(piso_puerta);
+            piso = true;
+        }
+
+        return (name || surname || piso || direction || postal_code);
+
+    }
+
 
 }
