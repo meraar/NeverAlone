75c3bb51ec70cdb6bfd1e17a071e88b9773d10ed
muhammad.meraj.arshad <muhammad.meraj.arshad@est.fib.upc.edu>
2020-11-18 22:38:40 +0100

    Foto de Perfil Implementada, Falla el get de la foto.

--- app/src/main/java/com/example/neveralone/Activity/UserProfileActivity.java
+++ app/src/main/java/com/example/neveralone/Activity/UserProfileActivity.java
@@ -1,11 +1,14 @@
 package com.example.neveralone.Activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,14 +19,21 @@ import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.neveralone.R;
 import com.example.neveralone.Usuario.Usuario;
 import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -31,9 +41,18 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
+
+import id.zelory.compressor.Compressor;
 
 public class UserProfileActivity<InputLayout> extends AppCompatActivity {
     private EditText nombretxt, apellidotxt, dnitxt, codigo_postaltxt, direcciontxt, emailtxt;
@@ -72,17 +91,112 @@ public class UserProfileActivity<InputLayout> extends AppCompatActivity {
         initialize2();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == CropImage.PICK_IMAGE_CHOOSER_REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            Uri imagen_uri = CropImage.getPickImageResultUri(this,data);
+
+            CropImage.activity(imagen_uri)
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setRequestedSize(640, 480)
+                    .setAspectRatio(2, 2).start(UserProfileActivity.this);
+        }
+
+        if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                Toast.makeText(UserProfileActivity.this, "Estoy dentro", Toast.LENGTH_SHORT).show();
+                File url = new File(resultUri.getPath());
+
+                Picasso.with(this).load(url).into(profile_image);
+                //Comprimir la imagen
+                try{
+                    bitmap = new Compressor(this).setMaxWidth(640).setMaxHeight(480).setQuality(90).compressToBitmap(url);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+                final byte [] bitmap_byte = byteArrayOutputStream.toByteArray();
+                final String foto_name = id + ".jpg";
+
+                final StorageReference ref = FirebaseStorage.getInstance().getReference().child("profilesImages").child(foto_name);
+                UploadTask uploadTask = ref.putBytes(bitmap_byte);
+
+                //Aqui se sube la imgen
+                /**Task<Uri> uniTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                    @Override
+                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                        if(!task.isSuccessful()){
+                            Toast.makeText(UserProfileActivity.this, "Error al subir la foto", Toast.LENGTH_SHORT).show();
+                        }
+                        return ref.getDownloadUrl();
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        Uri descargar_uri = task.getResult();
+                        reference_image.push().child("Fotos").child(id).setValue(descargar_uri.toString());
+                        cargando.dismiss();
+                        Toast.makeText(UserProfileActivity.this, "Imagen cargada correctamente", Toast.LENGTH_SHORT).show();
+                    }
+                });**/
+                ref.putBytes(bitmap_byte).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        getDownloadUrl(ref);
+                    }
+                });
+            }
+        }
+    }
+
+    private void getDownloadUrl(StorageReference ref) {
+        ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                UserProfileChangeRequest request = new UserProfileChangeRequest.Builder().setPhotoUri(uri).build();
+
+                user.updateProfile(request).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(UserProfileActivity.this, "Foto cargada correctamente", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(UserProfileActivity.this, "ERRROR: Foto no cargado", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
     private void initialize2() {
         profile_image = findViewById(R.id.profile_image);
-        reference_image = FirebaseDatabase.getInstance().getReference().child("Fotos");
+        reference_image = FirebaseDatabase.getInstance().getReference().child("Usuarios").child(id);
         storageReference = FirebaseStorage.getInstance().getReference().child("user_image");
         cargando = new ProgressDialog(this);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+
+
 
     }
 
     private void initialize(){
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         user = firebaseAuth.getCurrentUser();
+        String h = user.getEmail().toString();
+        Toast.makeText(UserProfileActivity.this, "HOLA "+ user.getPhotoUrl().toString()+" ", Toast.LENGTH_SHORT).show();
+        /**if (!user.getPhotoUrl().toString().isEmpty()){
+            Glide.with(this).load(user.getPhotoUrl().toString()).into(profile_image); // NO ESTA FUNCIONANDO
+        }**/
         id = user.getUid();
         reference = FirebaseDatabase.getInstance().getReference();
         reference.child("Usuarios").child(id).addListenerForSingleValueEvent(
