2f8c430dbca6060ab39166eefecc8f32237cb440
Marcel <marcel.dfga@gmail.com>
2020-11-30 14:03:06 +0100

    Añade los botones: Borrar, editar, aceptar en la vista a detalle de la peticion

--- app/src/main/java/com/example/neveralone/Activity/Peticiones/PeticionDetail.java
+++ app/src/main/java/com/example/neveralone/Activity/Peticiones/PeticionDetail.java
@@ -2,6 +2,8 @@ package com.example.neveralone.Activity.Peticiones;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +13,8 @@ import com.example.neveralone.R;
 
 public class PeticionDetail extends AppCompatActivity {
 
-    TextView categoria, fecha, hora, descripcion, estado, autor;
+    private TextView categoria, fecha, hora, descripcion, estado, autor;
+    private Button borrar,editar,aceptar;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,13 +26,21 @@ public class PeticionDetail extends AppCompatActivity {
     private void init() {
         Intent i = getIntent();
         Peticion p = (Peticion) i.getSerializableExtra("Peticion");
-        //Peticion n = new Peticion("Pepe","","Compras","12/11/20","18:30","Caca");
+        //Peticion p = new Peticion("Pepe","","Compras","12/11/20","18:30","Caca");
         categoria   = findViewById(R.id.CategoriaPeticion);
         fecha       = findViewById(R.id.fechaAct);
         descripcion = findViewById(R.id.contenidoDescripción);
         estado      = findViewById(R.id.estado);
         autor       = findViewById(R.id.CreadorPeticion);
         hora        = findViewById(R.id.horaAct);
+        borrar        = findViewById(R.id.borrar);
+        editar        = findViewById(R.id.editar);
+        aceptar        = findViewById(R.id.ofrecer);
+
+        editar.setText("Editar");
+        borrar.setText("Borrar");
+
+        aceptar.setVisibility(View.GONE);
 
         categoria.setText(p.getCategoria());
         fecha.setText(p.getFecha());
