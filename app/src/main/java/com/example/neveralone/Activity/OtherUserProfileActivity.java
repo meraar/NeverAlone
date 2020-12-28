d11ef99f4fc2b85821e9d10ee125083ca367b747
muhammad.meraj.arshad <muhammad.meraj.arshad@est.fib.upc.edu>
2020-12-28 16:48:25 +0100

    Update

--- app/src/main/java/com/example/neveralone/Activity/OtherUserProfileActivity.java
+++ app/src/main/java/com/example/neveralone/Activity/OtherUserProfileActivity.java
@@ -2,14 +2,21 @@ package com.example.neveralone.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.neveralone.Peticion.Peticion;
 import com.example.neveralone.R;
 
 public class OtherUserProfileActivity extends AppCompatActivity {
+
+    private String user_uid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other_user_profile);
+        Intent i     = getIntent();
+        user_uid     = (String) i.getSerializableExtra("uid");
     }
 }
